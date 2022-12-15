25affe828e0fa26faf58d946e5fca7e6b85588f7
garuca <gabriel.rufino.carvalho@gmail.com>
2022-12-15 18:36:49 -0300

    add dependence injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:list/features/content/list/presenter/ui/list_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:list/app_widget.dart';
+import 'package:list/features/content/list/domain/use_cases/list_content.dart';
+import 'package:list/features/content/list/presenter/content_cubit.dart';
 
 void main() {
-  runApp(const App());
-}
-
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const ListPage();
-      },
-    ),
-  ],
-);
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  final sl = GetIt.I;
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<ContentCubit>(
+        create: (BuildContext context) => ContentCubit(sl<ListContent>()),
       ),
-      routerConfig: _router,
-    );
-  }
+    ],
+    child: const AppWidget(),
+  ));
 }
