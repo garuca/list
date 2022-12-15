e2c51218862bfc4394c39cd8941e3edc4de3501c
garuca <gabriel.rufino.carvalho@gmail.com>
2022-12-14 22:19:16 -0300

    implement domain of list content.

---
+++ lib/features/content/list/domain/repositories/content_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:list/features/content/common/domain/entities/content.dart';
+import 'package:list/features/content/list/domain/errors/errors.dart';
+
+abstract class ContentRepository {
+  Future<Either<Failure, List<Content>?>> list();
+}
---
+++ lib/features/content/list/domain/use_cases/list_content.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:list/features/content/common/domain/entities/content.dart';
+import 'package:list/features/content/list/domain/errors/errors.dart';
+import 'package:list/features/content/list/domain/repositories/content_repository.dart';
+
+abstract class ListContent {
+  Future<Either<Failure, List<Content>?>> call();
+}
+
+class ListContentImpl implements ListContent {
+  final ContentRepository repository;
+
+  ListContentImpl(this.repository);
+
+  @override
+  Future<Either<Failure, List<Content>?>> call() async {
+    return repository.list();
+  }
+}
