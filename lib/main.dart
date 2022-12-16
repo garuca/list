4c43cdf9de53794055aeea5f5896faf9914fe1e4
garuca <gabriel.rufino.carvalho@gmail.com>
2022-12-15 23:14:02 -0300

    Config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:list/app_widget.dart';
 import 'package:list/features/content/list/domain/use_cases/list_content.dart';
 import 'package:list/features/content/list/presenter/content_cubit.dart';
+import 'package:list/firebase_options.dart';
 
 void main() {
   final sl = GetIt.I;
